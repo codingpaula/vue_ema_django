d403316a4b5c3d7db6eafdd66b4c762ac4a739ac
Manuela Ritter <manritter@uni-osnabrueck.de>
2018-06-06 12:51:11 +0200

    choices for colors

--- emapp/matrix/models.py
+++ emapp/matrix/models.py
@@ -10,7 +10,46 @@ class TimeStamped(models.Model):
 
 class Topic(TimeStamped):
     name = models.CharField(verbose_name='Topic Name', max_length=50)
-    color = models.CharField(verbose_name='Color', max_length=6)
+    GREY = '4d4e4e'
+    DARKBLUE = '001f54'
+    BLUE = '1f487e'
+    LIGHTBLUE = '59a5d8'
+    LIGHTBROWN = '926639'
+    BROWN = '6f5232'
+    TURQUOISE = '048ba8'
+    DARKGREEN = '137547'
+    GREEN = '528029'
+    LIGHTGREEN = '8ea604'
+    YELLOW = 'eabc20'
+    ORANGE = 'f56416'
+    RED = 'e71d36'
+    DARKRED = '671934'
+    PINK = 'd90368'
+    PURPLE = 'a4036f'
+    COLOR_OPTIONS = [
+        (GREY, 'grey'),
+        (DARKBLUE, 'dark blue'),
+        (BLUE, 'blue'),
+        (LIGHTBLUE, 'light blue'),
+        (LIGHTBROWN, 'light brown'),
+        (BROWN, 'brown'),
+        (TURQUOISE, 'turquoise'),
+        (DARKGREEN, 'dark green'),
+        (GREEN, 'green'),
+        (LIGHTGREEN, 'light green'),
+        (YELLOW, 'yellow'),
+        (ORANGE, 'orange'),
+        (RED, 'red'),
+        (DARKRED, 'dark red'),
+        (PINK, 'pink'),
+        (PURPLE, 'purple'),
+    ]
+    # yellow, orange, red, pink, rose, purple, turquoise, blue, darkblue, darkred
+
+    color = models.CharField(verbose_name='Color',
+        max_length=6,
+        choices=COLOR_OPTIONS,
+        default=GREY)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Task(TimeStamped):
